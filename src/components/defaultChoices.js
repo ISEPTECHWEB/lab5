5639878a0bfab5c225b21c64c1a70b14651a368a
theohugues <theo.hugues@gmail.com>
2017-12-18 20:24:39 +0100

    survey_v2

--- src/components/defaultChoices.js
+++ src/components/defaultChoices.js
@@ -1,7 +1,4 @@
 const choices = [
-    {id: 1, name: 'blue or light blue', vote:2, pourcent:0.01},
-    {id: 2, name: 'green', vote:4, pourcent:0.01},
-    {id: 3, name: 'yellow', vote:1, pourcent:0.01}
 ]
 
 export default choices;
